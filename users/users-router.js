30098fb923052e5b7426baef96b5738280cf8d5d
BrannanC <brannanconrad@gmail.com>
2019-09-15 21:33:34 -0500

    Shaped our user data better

--- users/users-router.js
+++ users/users-router.js
@@ -1,42 +1,36 @@
 const router = require("express").Router();
 
 const Users = require("./users-model.js");
-const Photos = require("../photos/photos-model")
+const Photos = require("../photos/photos-model");
 // const restricted = require("../middleware/restricted-middleware.js");
-
 
 // ---------------------- /api/users ---------------------- //
 
-
 router.get("/", (req, res) => {
-    Users.getAllUsers()
-        .then(users => {
-            res.status(200).json(users);
-        })
-        .catch(err => {
-            res.status(500).json(err);
-        });
+  Users.getAllUsers()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
-
 
 // ---------------------- GET User By Id ---------------------- //
 
 router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await Users.getUserById(id);
+    user.photos = await Users.getPhotosByUserId(id);
+    user.favorites = await Photos.getLikedPhotosByUserId(id);
+    delete user.password;
 
-    try {
-        const id = req.params.id;
-        const user = await Users.getUserById(id)
-        const photos = await Users.getPhotosByUserId(id)
-        const favorites = await Photos.getLikedPhotosByUserId(id)
-        delete user.password;
-
-        res.status(200).json({ user, photos, favorites });
-    } catch (error) {
-        res.status(500).json({ error })
-    }
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
-
-
 
 // ---------------------- Custom Middleware ---------------------- //
 
